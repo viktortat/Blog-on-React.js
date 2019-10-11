709166e999754f80ca398d9a4ba4f429ca2e48ab
Ilyacheredov1 <53446839+Ilyacheredov1@users.noreply.github.com>
2019-10-12 00:36:38 +0300

    Add files via upload

---
+++ precache-manifest.67f07d75f0a1168da9a633ee73d3e687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ec8bc8a0c103e0cd93d6a3147fe60b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c2a478c9d084139640",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5808e9b8954b52855454",
+    "url": "/static/css/main.d1ae1d63.chunk.css"
+  },
+  {
+    "revision": "98c2a478c9d084139640",
+    "url": "/static/js/2.1f22437a.chunk.js"
+  },
+  {
+    "revision": "5808e9b8954b52855454",
+    "url": "/static/js/main.612b896c.chunk.js"
+  },
+  {
+    "revision": "14b990a1904df628e6c5",
+    "url": "/static/js/runtime-main.7d809263.js"
+  },
+  {
+    "revision": "0ed0afbc11bc346c67763d24fb696327",
+    "url": "/static/media/background1.0ed0afbc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.04e61db3e751cc401572f495bb205f92.js"
+  "/precache-manifest.67f07d75f0a1168da9a633ee73d3e687.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
